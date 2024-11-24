f03d442177d7d79588710a2daeec99a27bfb34c9
Sahil Choudhary <sahilchoudhary@Sahils-MacBook-Air.local>
2024-11-24 18:43:05 +0530

    adding rate limiters

---
+++ src/main/java/com/learn/leetcodeproblems/SlidingWindowLogRateLimiterMain.java
@@ -0,0 +1,40 @@
+package com.learn.leetcodeproblems;
+
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+
+class SlidingWindowLogRateLimiter {
+    int tokens, windowInSecs;
+    Deque<Instant> logSet;
+    SlidingWindowLogRateLimiter (int tokens, int windowInSecs) {
+        this.tokens = tokens;
+        this.windowInSecs = windowInSecs;
+        logSet = new LinkedList<>();
+    }
+
+    boolean allowRequest() {
+        Instant curTime = Instant.now();
+        logSet.removeIf(log -> Duration.between(log, curTime).getSeconds() > windowInSecs);
+        if (logSet.size() < tokens) {
+            logSet.offer(curTime);
+            return true;
+        }
+        return false;
+    }
+}
+
+public class SlidingWindowLogRateLimiterMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        SlidingWindowLogRateLimiter rateLimiter = new SlidingWindowLogRateLimiter(
+                2, 10);
+        for (int i = 0; i < 100; i++) {
+            boolean allowed = rateLimiter.allowRequest();
+            System.out.println("RequestId " + i + " Allowed = " + allowed);
+            Thread.sleep(500);;
+        }
+    }
+
+}
---
+++ src/main/java/com/learn/leetcodeproblems/TokenBucketRateLimiterMain.java
@@ -0,0 +1,51 @@
+package com.learn.leetcodeproblems;
+
+
+import java.time.Duration;
+import java.time.Instant;
+
+class TokenBucketRateLimiter {
+    int maxCapacity;
+    int refillRate;
+    int tokens;
+    Instant lastRefillTime;
+
+    TokenBucketRateLimiter(int tokens, int refillRate) {
+        this.maxCapacity = tokens;
+        this.tokens = tokens;
+        this.refillRate = refillRate; // considering this is PER SECOND only
+        this.lastRefillTime = Instant.now();
+    }
+
+
+    public boolean allowRequest() {
+        Instant tokenRequestedAt = Instant.now();
+        int elapsedTime = (int) (tokenRequestedAt.getEpochSecond() - lastRefillTime.getEpochSecond());
+        elapsedTime = Math.max(elapsedTime, 0);
+        int tokensToAdd = elapsedTime * refillRate;
+        this.tokens = Math.min(tokens + tokensToAdd , maxCapacity);
+        System.out.println("Elapsed time is " + elapsedTime +
+                " current tokens at allow request is " + this.tokens);
+        if (this.tokens > 0) {
+            tokens --;
+            System.out.println("Consumed token successfully, remaining tokens " + this.tokens);
+            lastRefillTime = lastRefillTime.plusSeconds(tokens / refillRate);
+            return true;
+        } else {
+            System.out.println("No token left");
+            return false;
+        }
+    }
+
+}
+
+public class TokenBucketRateLimiterMain {
+    public static void main(String[] args) throws InterruptedException {
+        TokenBucketRateLimiter rateLimiter = new TokenBucketRateLimiter(10, 5);
+        for (int i = 0; i < 50; i ++) {
+            System.out.println("Requesting token availability for request:" + i);
+            rateLimiter.allowRequest();
+            Thread.sleep(500);
+        }
+    }
+}
