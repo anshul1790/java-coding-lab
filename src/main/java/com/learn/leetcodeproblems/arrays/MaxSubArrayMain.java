ace352b7021b82f4b70f336f55d8ca4065335452
Anshul Choudhary <sahilchoudhary@Sahils-MacBook-Air.local>
2024-12-04 23:18:13 +0530

    add old probs

---
+++ src/main/java/com/learn/leetcodeproblems/arrays/MaxSubArrayMain.java
@@ -0,0 +1,19 @@
+package com.learn.leetcodeproblems.arrays;
+
+public class MaxSubArrayMain {
+    public static void main(String[] args) {
+        maxSubArray(new int[] {2, 3, -8, 7, -1, 2, 3});
+    }
+    // input -> [-2,1,-3,4,-1,2,1,-5,4]
+    // output -> 6, [4,-1,2,1]
+    static void maxSubArray(int[] array) {
+        int resultMax = array[0];
+        int currentMax = array[0];
+        for (int i = 1; i < array.length; i++) {
+            currentMax = Math.max(array[i], currentMax + array[i]);
+
+            resultMax = Math.max(resultMax, currentMax);
+        }
+        System.out.println(resultMax);
+    }
+}
---
+++ src/main/java/com/learn/leetcodeproblems/arrays/MergeSortedArrayMain.java
@@ -0,0 +1,26 @@
+package com.learn.leetcodeproblems.arrays;
+
+import java.util.Arrays;
+
+public class MergeSortedArrayMain {
+    public static void main(String[] args) {
+        mergeSortedArray(new int[]{1, 7, 10, 11}, new int[]{1, 3, 6, 9, 45, 50});
+    }
+
+    // [1, 5, 8, 10] and [2, 3, 6, 9, 12, 15]
+    // [1, 2, 3, 5, 6, 8, 10, 12, 15]
+    // [1, 2, 3, 4] [5, 6, 7, 8, 9, 10]
+    static void mergeSortedArray(int[] array1, int[] array2) {
+        int leftListPointer = 0; int rightListPointer = 0; int index = 0;
+        int[] newArr = new int[array1.length + array2.length];
+        while (leftListPointer < array1.length && rightListPointer < array2.length) {
+            if (array1[leftListPointer] < array2[rightListPointer]) {
+                newArr[index++] = array1[leftListPointer++];
+            } else {
+                newArr[index++] = array2[rightListPointer++];
+            }
+        }
+        while (leftListPointer < array1.length) { newArr[index++] = array1[leftListPointer++]; }
+        while (rightListPointer < array2.length) { newArr[index++] = array2[rightListPointer++]; }
+        System.out.println(Arrays.toString(newArr)); }
+}
