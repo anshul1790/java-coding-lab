652491c8924ba3b3551ab492a778c9cc77e2ed66
Anshul Choudhary <sahilchoudhary@Sahils-MacBook-Air.local>
2024-12-05 16:40:44 +0530

    linked list

---
+++ src/main/java/com/learn/leetcodeproblems/arrays/MoveZerosMain.java
@@ -0,0 +1,25 @@
+package com.learn.leetcodeproblems.arrays;
+
+import java.util.Arrays;
+
+public class MoveZerosMain {
+    public static void main(String[] args) {
+        moveZeroes(new int[]{1, 0, 3, 4});
+    }
+
+    // [1,0,3,12,0]
+    // [1,0,3,12,0]
+    //Output: [1,3,12,0,0]
+    static void moveZeroes(int[] arr) {
+        int newIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                arr[newIndex++] = arr[i];
+            }
+        }
+        while (newIndex < arr.length) {
+            arr[newIndex++] = 0;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
