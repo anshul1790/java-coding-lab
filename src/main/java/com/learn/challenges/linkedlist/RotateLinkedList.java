8b8cf31f22d386db07e232de0fecf40c40d89880
Anshul Choudhary <sahilchoudhary@Sahils-MacBook-Air.local>
2024-12-06 11:55:33 +0530

    ll examples

--- src/main/java/com/learn/challenges/linkedlist/RotateLinkedList.java
+++ src/main/java/com/learn/challenges/linkedlist/RotateLinkedList.java
@@ -5,7 +5,7 @@ public class RotateLinkedList {
     static Node createLinkedList() {
         Node head = new Node(1);
         Node current = head;
-        for(int i = 2; i <= 4; i ++) {
+        for(int i = 2; i <= 5; i ++) {
             current.nextNode = new Node(i);
             current = current.nextNode;
         }
@@ -14,12 +14,26 @@ public class RotateLinkedList {
 
     public static void main(String[] args) {
         Node linkedList = createLinkedList();
-        rotateLinkedList(linkedList, 1);
+        rotateLinkedList(linkedList, 4);
     }
 
     /* Input: linked list = 10 -> 20 -> 30 -> 40 -> 50, k = 4
     Output: 50 -> 10 -> 20 -> 30 -> 40*/
     static void rotateLinkedList(Node head, int k) {
-
+        Node current = head;
+        int len = 1;
+        while (current.nextNode != null) {
+            current = current.nextNode;
+            len++;
+        }
+        k = k % len;
+        current.nextNode = head;
+        while (k > 0) {
+            current = current.nextNode;
+            k--;
+        }
+        head = current.nextNode;
+        current.nextNode = null;
+        System.out.println(head);
     }
 }
